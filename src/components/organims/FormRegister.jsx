241e6092a2cb78a25b92ee46965612dcfa3db8ea
Valex4 <yam778123@gmail.com>
2023-05-27 11:55:28 -0600

    [ADD] forms login and register

--- src/components/organims/FormRegister.jsx
+++ src/components/organims/FormRegister.jsx
@@ -13,20 +13,19 @@ function FormRegister() {
           try {
              const response = await createUser(values);
             console.log(values);
-            console.log("registrado exitosamente como estudiante")
             alert("registrado exitosamente como estudiante");
             navigate("/login") 
           } catch (error) {
             console.log(error);
           }
             }}
-            > {({ handleChange,errors, handleSubmit, isSubmitting }) => (
+            >{({ handleChange,errors, handleSubmit, isSubmitting }) => (
                 <Form onSubmit={handleSubmit} className="form-registro">
                    <h3 className="register-title">Registrate</h3>
                    <div className='inputs-v1'>
                   <WrapperInput   name="name"  type="text" onChange={handleChange} msn="Nombre" placeholder="Nombre" errors={errors.name} CNH1={errors.name == "Obligatorio" ? "error" : ""}/> 
                    </div>
-                   <WrapperInput   name="mail"  type="text" onChange={handleChange} msn="Correo electronico" placeholder="Dirección de correo electronico" errors={errors.email} CNH1={errors.mail == "Obligatorio" || errors.mail == "Por favor ingresa la direccion correctamente" ? "error" : ""} />
+                   <WrapperInput   name="Email"  type="text" onChange={handleChange} msn="Correo electronico" placeholder="Dirección de correo electronico" errors={errors.email} CNH1={errors.mail == "Obligatorio" || errors.mail == "Por favor ingresa la direccion correctamente" ? "error" : ""} />
                   <WrapperInput name="password"  type="password" onChange={handleChange} msn="Contraseña" placeholder="Contraseña" errors={errors.password} CNH1={errors.password == "Obligatorio" || errors.password == "Porfavor ingresa una contraseña más segura" || errors.password == "password must be at least 5 characters" ? "error" : ""}/>   
                   <WrapperInput name="confirmPassword"  type="password" onChange={handleChange} msn="Validar Contraseña" placeholder="Validar Contraseña" errors={errors.confirmPassword} CNH1={errors.confirmPassword == "Obligatorio" || errors.confirmPassword == "Las contraseñas no coinciden" ? "error" : ""} />  
                   <div className="btn-registro">
